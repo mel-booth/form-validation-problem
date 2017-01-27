77d036cbf24c36953a0f495d991e91ef34e74ac1
Mel Booth <mel.m.booth@gmail.com>
2017-01-27 14:11:19 +1300

    remove App component - unnecessary

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
-import App from './components/app'
+
 import Form from './components/form'
 
 render(
-<div>
-<App name='form-validation-react' />
-<Form />
-</div>
-, document.querySelector('main'))
+<Form />, document.querySelector('main'))
 console.log('welcome to form-validation-react')
