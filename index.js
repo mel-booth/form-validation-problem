e52a526ca71c3360ced3ea85cd875517f99b98bf
Mel Booth <mel.m.booth@gmail.com>
2017-01-27 14:03:28 +1300

    create form component - renders successfully

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './components/app'
+import Form from './components/form'
 
-render(<App name='form-validation-react' />, document.querySelector('main'))
+render(
+<div>
+<App name='form-validation-react' />
+<Form />
+</div>
+, document.querySelector('main'))
 console.log('welcome to form-validation-react')
